4d91f1b7a5aed4cc5220f94be390d7d25e4094aa
Shubham Jha <shubhamjha271005@gmail.com>
2026-08-18 01:24:50 +0530

    Time: 12 ms (5.23%), Space: 263.3 MB (6.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
         int n = nums.size();
         k = k%n;
-        for(int i=0; i<n-k; i++){
-            nums.push_back(0);
-        }
-        int x = 0;
-        for(int i=n; i<2*n-k; i++){
-            nums[i] = nums[x];
-            x++;
-        }
-        nums.erase(nums.begin(), nums.begin()+n-k);
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+
+
+
+
+
+
+
+        // int n = nums.size();
+        // k = k%n;
+        // for(int i=0; i<n-k; i++){
+        //     nums.push_back(0);
+        // }
+        // int x = 0;
+        // for(int i=n; i<2*n-k; i++){
+        //     nums[i] = nums[x];
+        //     x++;
+        // }
+        // nums.erase(nums.begin(), nums.begin()+n-k);
         
     }
 };
