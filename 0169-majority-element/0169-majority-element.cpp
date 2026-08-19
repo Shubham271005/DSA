9ea96149587a665a59ba2e631aa3e33a85b95dde
Shubham Jha <shubhamjha271005@gmail.com>
2026-08-20 03:01:25 +0530

    Time: 0 ms (100%), Space: 28.1 MB (95.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,34 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return nums[(n-1)/2];
+
+            // moore voting algo
+            // approach is that suppose the array is divided into many subarrays, the majority element will be in majority in atleast one of the subarrays. 
+            // when cnt becomes 0, it means till that part there is no majority element, as our supposed ma got cancelled off .
+
+            int n = nums.size();
+            int ma = 0;
+            int cnt = 0;
+
+            for(int i=0; i<n; i++){
+                if(cnt == 0){
+                    ma = nums[i];
+                }
+                if(nums[i] == ma){
+                    cnt++;
+                } else {
+                    cnt--;
+                }
+            }
+
+            return ma;
+
+        
+
+        
+
+    //     int n = nums.size();
+    //     sort(nums.begin(), nums.end());
+    //     return nums[(n-1)/2];
     }
 };
